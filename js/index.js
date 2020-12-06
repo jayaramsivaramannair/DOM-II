c9a06945d1ace6fb4e40f0771993c65d4a5853e2
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2020-12-06 07:28:11 -0500

    Added mouseover event to title image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+//mouseover - increase title image
+//keydown - change the background color and font color
+//Load - Make something happen when the window loads
+//wheel - scale the title with wheel event
+//focus - use for sign me up buttons
+//resize - use for the window
+//select - when a text has been selected
+//dbl click - when an image has been double clicked
+//drag & drop - image swap
+
+//Make the title image a little transparent when mouseover event occurs
+let titleImage = document.querySelector('img');
+function transparentImage(event) {
+    event.target.style.opacity = 0.7;
+}
+titleImage.addEventListener('mouseover', transparentImage);
+
+
+
