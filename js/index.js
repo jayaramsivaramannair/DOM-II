13d0c76418873746a5c7633dc33e4532df2f348e
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2020-12-06 10:05:27 -0500

    Added an effect to dynamically change background on scroll

--- js/index.js
+++ js/index.js
@@ -46,6 +46,25 @@ window.addEventListener('load', function(e) {
     mainHeading.style.color = 'coral';
 });
 
+//Alert the user when the window is being resized
+window.addEventListener('resize', function(e) {
+    alert('You are a developer trying to view our website at different widths!')
+})
+
+//Dynamically change the background color. Gets darker as we scroll down.
+window.addEventListener('scroll', () => {
+    let red = 255, green = 127, blue = 80;
+    let y = (window.scrollY || windowpageYOffset) / 200;
+    y = (y < 1 ? 1 : y);
+    red = Math.round(red / y);
+    green = Math.round(green / y);
+    blue = Math.round(blue / y);
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    document.body.style.color = 'white';
+})
+
+/* SOURCE: https://stackoverflow.com/questions/52637835/dynamically-change-background-color-on-scroll */
+
 
 
 
